b0ddae9befbff7f9fec804bf1d1f08b91e4e47e8
Reddi.Abhinay <106007724+Abhinay3010@users.noreply.github.com>
2026-01-22 17:30:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-cluster-agent-ui/', // <-- important for GitHub Pages
+  base: '/ai-cluster-agent-ui/', // important for GitHub Pages
   plugins: [react()]
 })
